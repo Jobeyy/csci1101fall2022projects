c173e89f0cd93d2d7891124b700c1d790f4cbb20
Jobey Farias <JobeyFarias@gmail.com>
2022-11-22 22:21:28 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (728, 1, 'Jolly good show!');
+
+--UPDATE sandbox SET double_value = -728 WHERE id = 188;
+
+--DELETE FROM sandbox WHERE id = 182;
+
+SELECT * FROM sandbox;
